8e5fd8f947540a0cdebc50395c1de47413400767
LWaetzig <lucas@waetzig.net>
2023-06-26 00:17:55 +0200

    remove unnecessary code

--- src/model_creation.py
+++ src/model_creation.py
@@ -14,30 +14,14 @@ train_csv = pd.read_csv(os.path.join("data", "augmented_train.csv"), index_col=0
 test_data = os.path.join("data", "test")
 test_csv = pd.read_csv(os.path.join("data", "test.csv"), index_col=0)
 
-
 # Data preprocessing
 processor = ImageProcessor()
-
-images = list()
-labels = list()
-
-for i, row in tqdm(train_csv.iterrows()):
-    path = os.path.join(row["Path"])
-    label = row["classId"]
-    image = processor.preprocess_images(
-        image_path=path, image_size=(32, 32), convert_to_grayscale=False, sharpen=True
-    )
-    images.append(image)
-    labels.append(label)
-
-images = np.array(images)
-labels = np.array(labels)
-
-# Split data into train and validation set
-X_train, X_val, y_train, y_val = train_test_split(
-    images, labels, test_size=0.2, random_state=42
-)
+# train and validation data preprocessing
+X_train, X_val, y_train, y_val = processor.create_dataset(train_csv, train_split=True)
+# test data preprocessing
+test_images, test_labels = processor.create_dataset(test_csv, train_split=False)
 num_classes = len(set(y_train))
+
 
 # Create model
 model = Model(model_name="model_3")
@@ -46,34 +30,9 @@ model.train_model(X_train, y_train, epochs=10, batch_size=32, X_val=X_val, y_val
 model.save_model(model_path="models")
 
 
-# Test model
-test_data = os.path.join("data", "test")
-test_csv = pd.read_csv(os.path.join("data", "test.csv"), index_col=0)
-
+# Evaluate Model
 model = Model(model_name="model_3_test")
 model.load_model("models/model_2.h5")
 
-processor = ImageProcessor()
-
-test_images = list()
-test_labels = list()
-for i , row in tqdm(test_csv.iterrows()):
-    path = os.path.join(row["Path"])
-    label = row["classId"]
-    image = processor.preprocess_images(
-        image_path=path, image_size=(32, 32), convert_to_grayscale=False, sharpen=True
-    )
-    image = np.expand_dims(image, axis=0)
-    test_images.append(image)
-    test_labels.append(label)
-
-test_images = np.array(test_images)
-test_labels = np.array(test_labels)
-
 model.evaluate(test_images, test_labels)
 model.save_model(model_path="models")
-
-model.accuracy
-model.precision
-model.recall
-model.f1_scor
